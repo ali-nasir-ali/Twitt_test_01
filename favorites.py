420c26d22c76d0b6e5125d9211443ad624e28c5d
anasir514 <anasir514@gmail.com>
2021-05-15 21:49:26 +0100

    d

--- favorites.py
+++ favorites.py
@@ -12,9 +12,11 @@ auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-tweet_list = api.favorites(screen_name='DavidDobrik', count=5)
+tweet_list = api.favorites(screen_name='DavidDobrik', count=2)
 # print(tweet_list)
 
 # print the id for each of the tweets
 for tweet in tweet_list:
     print(tweet.id)
+    tweet_id = tweet.id
+    api.retweet(tweet_id)
--- tweepy_test.py
+++ tweepy_test.py
@@ -1,6 +1,6 @@
-import tweepy
+import tweepy   # pip install tweepy
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # pip install python-dotenv
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
@@ -12,4 +12,12 @@ auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status(status="Hello World.")
+# api.update_status(status="Hello World.")
+user = api.get_user('Lenniesaurus')
+print(user.screen_name)
+#print(user.friends(*, user_id, screen_name, cursor, count, skip_status, include_user_entities))
+"""
+user = api.get_user('NasirTheRoman')
+print(user.screen_name)
+print(user.followers_count)
+"""
