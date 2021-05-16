fc507e1a9840482560cb442cd0965fbc9678891b
anasir514 <anasir514@gmail.com>
2021-05-16 16:35:34 +0100

    d

--- favorites.py
+++ favorites.py
@@ -12,7 +12,7 @@ auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-tweet_list = api.favorites(screen_name='DavidDobrik', count=2)
+tweet_list = api.favorites(screen_name='DavidDobrik', count=5)
 # print(tweet_list)
 
 # print the id for each of the tweets
