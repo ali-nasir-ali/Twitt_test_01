e2eab0c3a077b0df6f0a228d5cd380a60b3191ba
anasir514 <anasir514@gmail.com>
2021-05-16 16:37:18 +0100

    x

--- retweet_bot.py
+++ retweet_bot.py
@@ -2,10 +2,8 @@ import tweepy
 import os
 from services import tweepy_auth, retweet_favorites
 from dotenv import load_dotenv
-load_dotenv(vddddddd)
-ad_dotenv
-load_dotenv(vddddddd)ad_dotenv
-load_dotenv(vddddddd)
+load_dotenv()
+
 API_KEY = os.getenv('API_KEY')
 API_SECRET = os.getenv('API_SECRET')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
